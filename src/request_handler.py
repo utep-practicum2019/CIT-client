9528a3629d56a4141b51c8c591da6b16c98231a2
apalomino2 <apalomino2@miners.utep.edu>
2019-03-03 21:54:33 -0700

    Added list platforms method

--- src/request_handler.py
+++ src/request_handler.py
@@ -17,6 +17,15 @@ class RequestHandler:
         return r.json()
 
     def get_platform(platform_id):
-        data = {'platform_id':platform_id}
+        data = {'platform_name':platform_id}
         r = requests.get(RequestHandler.__domain + RequestHandler.__platform_path, json=data)
         return r.json()
+
+    def list_plarforms(user_id):
+        # TODO: test/fix this method, should list all platforms for a group
+        group = RequestHandler.get_group(user_id)
+        tmp = {}
+        for p in group['platforms']:
+            plat = RequestHandler.get_platform(p)
+            tmp[p] = p['subplatforms']
+        return tmp
